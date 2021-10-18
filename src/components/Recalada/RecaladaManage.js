d7beaf2e8f46dc114c3135dff5de5d8f421ff10a
olivercera <olivercera@gmail.com>
2021-10-18 17:11:02 -0500

    1 file modified

--- src/components/Recalada/RecaladaManage.js
+++ src/components/Recalada/RecaladaManage.js
@@ -4,28 +4,30 @@ import './Recalada.css';
 export default function RecaladaManage() {
   return (
     <div>
-      Action
-      <div class="select">
-        <select>
-          <option>Select dropdown</option>
-          <option>With options</option>
-        </select>
-      </div>{' '}
-      Terminal
-      <div class="select">
-        <select>
-          <option>Select dropdown</option>
-          <option>With options</option>
-        </select>
-      </div>{' '}
-      Product
-      <div class="select">
-        <select>
-          <option>Select dropdown</option>
-          <option>With options</option>
-        </select>
-      </div>{' '}
-      <a>Cancel</a> <button>Add Recalada</button>
+      <form onSubmit={handleSubmit}>
+        Action
+        <div class="select">
+          <select>
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </div>{' '}
+        Terminal
+        <div class="select">
+          <select>
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </div>{' '}
+        Product
+        <div class="select">
+          <select>
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </div>
+        <a>Cancel</a> <button type="submit">Add Recalada</button>
+      </form>
     </div>
   );
 }
